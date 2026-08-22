4876dc98dd963c5c1e040020d824a6a2722d83b9
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-08-23 01:24:35 +0530

    feat: route phones to mobile app UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import MobileDashboard from './pages/MobileDashboard';
 import RecordsPage from './pages/RecordsPage';
 import RecordDetailPage from './pages/RecordDetailPage';
+
+function ResponsiveDashboard() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia('(max-width: 767px)');
+    const update = () => setIsMobile(media.matches);
+    update();
+    media.addEventListener?.('change', update);
+    return () => media.removeEventListener?.('change', update);
+  }, []);
+
+  return isMobile ? <MobileDashboard /> : <Dashboard />;
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<ResponsiveDashboard />} />
       <Route path="/records" element={<RecordsPage />} />
       <Route path="/records/:id" element={<RecordDetailPage />} />
     </Routes>
