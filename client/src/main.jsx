a1fb7338c5f8d1ab883d898388afa32ff7815ade
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-08-23 21:53:28 +0530

    Enable app serial and PDF enhancements

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './utils/appEnhancements';
 import App from './App.jsx';
 import './index.css';
 
