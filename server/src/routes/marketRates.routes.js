ef69b1e64581ae97374a692cef45c1ebde0be207
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-08-24 00:37:52 +0530

    Remove retired market-rate route

--- server/src/routes/marketRates.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMarketRates } from '../controllers/marketRates.controller.js';
-
-const router = Router();
-
-router.get('/', getMarketRates);
-
-export default router;
