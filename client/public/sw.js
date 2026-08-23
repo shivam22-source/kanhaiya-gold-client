d08dae44e69c8da418b2f8523773ccf84e6fa533
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-08-24 01:01:27 +0530

    Bump PWA cache after dashboard changes

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kanhaiya-gold-shell-v2';
+const CACHE_NAME = 'kanhaiya-gold-shell-v3';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', (event) => {
