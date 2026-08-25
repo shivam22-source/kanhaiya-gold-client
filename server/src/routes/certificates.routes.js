4ccc1b7e725f93aa1ba6600b4e36ca3d80660dc1
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-08-26 02:57:31 +0530

    Add certificate delete endpoint

--- server/src/routes/certificates.routes.js
+++ server/src/routes/certificates.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createCertificate, getCertificate, getNextSerial, listCertificates } from '../controllers/certificates.controller.js';
+import { createCertificate, deleteCertificate, getCertificate, getNextSerial, listCertificates } from '../controllers/certificates.controller.js';
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.post('/', createCertificate);
 router.get('/', listCertificates);
 router.get('/next-serial', getNextSerial);
 router.get('/:id', getCertificate);
+router.delete('/:id', deleteCertificate);
 
 export default router;
