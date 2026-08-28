00a62608d589ca5d80d4ee3c9b201a4f89480da6
Shivam Thakur <b424056@iiit-bh.ac.in>
2026-08-28 15:21:43 +0530

    Add payment transaction endpoints

--- server/src/routes/dues.routes.js
+++ server/src/routes/dues.routes.js
@@ -4,14 +4,18 @@ import {
   deleteDue,
   listDueCandidates,
   listDues,
+  listPayments,
+  recordPayment,
   updateDue,
 } from '../controllers/dues.controller.js';
 
 const router = Router();
 
 router.get('/candidates', listDueCandidates);
+router.get('/certificate/:certificateId/payments', listPayments);
 router.get('/', listDues);
 router.post('/', createDue);
+router.post('/:certificateId/payments', recordPayment);
 router.put('/:certificateId', updateDue);
 router.delete('/:certificateId', deleteDue);
 
